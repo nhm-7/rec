d1bc56d69be73d5a8a9775303f75487b3b98fb3c
nhormann <nicolas.hormann@fligoo.com>
2022-11-23 20:24:33 -0300

    remove args from base_exp

--- code/rec/experiments/base.py
+++ code/rec/experiments/base.py
@@ -85,8 +85,7 @@ def get_data_loaders(
 
 @experiment_component
 def base_experiment(
-    model_factory: Any, model_args: Dict = None, loss_args: Dict = None,
-    trainer_args: Dict = None, runtime_args: Dict = None
+    model_factory: Any, trainer_args: Dict = None, runtime_args: Dict = None
     ) -> None:
     pl.seed_everything(runtime_args["seed"])
     transformers.logging.set_verbosity_error()
