7becd51845d3d096c60082952dadce1b339e46cf
Nicolas Hormann Manriquez <nhormann@Nabu2>
2023-04-02 23:49:31 -0300

    add exp_00

--- code/rec/experiments/exps.py
+++ code/rec/experiments/exps.py
@@ -220,3 +220,62 @@ def exp_003():
 def exp_004():
     """Define an exp that is the same as the paper (referit->baseline), but shuting down the visual embeddings."""
     run_experiment(model_factory=lit_model_factory)
+
+
+@experiment({
+    "model_args": {
+        "backbone": "resnet50",
+        "mask_pooling": False,
+        "dropout_p": 0.1,
+        "num_heads": 8,
+        "num_layers": 6,
+        "num_conv": 0,
+        "use_visual_embeddings": True,
+    },
+    "data_args": {
+        "dataset": "refclef",
+        "max_length": 32,
+        "input_size": 512,
+    },
+    "loss_args": {
+        "beta": 0.1,
+        "gamma": 0.1,
+        "mu": 0.0,
+    },
+    "trainer_args": {
+        "learning_rate": 0.0001,
+        "weight_decay": 0.0,
+        "batch_size": 12,
+        "grad_steps": 4,
+        "max_epochs": 120,
+        "scheduler": lambda max_epochs: {
+            'milestones': [int(p * max_epochs) for p in (0.6, 0.9)],
+            'gamma': 0.1
+        }
+    },
+    "runtime_args": {
+        "gpus": "0,1,2",
+        "num_workers": 20,
+        "seed": 3407,
+        "suffix": None,
+        "cache": "./cache",
+        "debug": False,
+        "early_stopping": False,
+        "amp": False,
+        "force_ddp": False,
+        "profile": False,
+        "checkpoint": True,
+        "save_last": True,
+        "pdata": 1.0,
+        "output_dir": "exp_005",
+        "get_sample": False
+    }
+})
+def exp_005():
+    """Define an exp that is the same as the paper (referit->baseline).
+
+    Is the 20211220_201458 prefix folder name from paper's google drive versioned models.
+    The idea for this experiment is to compare its results with the reported ones in the paper.
+    We use more epochs (+30) in this experiment. That's the difference with exp_003.
+    """
+    run_experiment(model_factory=lit_model_factory)
