499abe89acfad3fae87e4f360fac45287e6d2a22
Nicolas Hormann - <nicolas.hormannmanriquez@gmail.com>
2023-09-10 22:58:10 -0300

    update script

--- tools/run_mendieta.sh
+++ tools/run_mendieta.sh
@@ -3,27 +3,30 @@
 #SBATCH --output=slurm-%x.%j.out # %j job id, ½x job name
 #SBATCH --error=slurm-%x.%j.err
 #SBATCH --partition=multi
-#SBATCH --nodes=1                # node count
-#SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=10       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --gres=gpu:1             # number of gpus per node
-#SBATCH --time=1-24:00           # total run time limit (<days>-<hours>:<minutes>)
-
+#SBATCH --nodes=2                # node count
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=8       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --time=0-00:30           # total run time limit (<days>-<hours>:<minutes>)
+#. /etc/profile
+module purge
+ulimit -c unlimited  # core dump
+ulimit -s unlimited  # stack
 
 echo "job \"${SLURM_JOB_NAME}\""
 echo "  id: ${SLURM_JOB_ID}"
 echo "  partition: ${SLURM_JOB_PARTITION}"
 echo "  node(s): ${SLURM_JOB_NODELIST}"
+echo " gres: ${SBATCH_GRES}"
+echo " gpus: ${SBATCH_GPUS}"
 date +"start %F - %T"
 echo ""
-
-#. /etc/profile
 
 source ${HOME}/.bashrc
 conda activate rec-env
 
 export TRANSFORMERS_OFFLINE=1
+export NCCL_DEBUG=INFO
 
 cd ${HOME}/rec/code/rec/
 
-srun yaer run -e exp_001
+yaer run -e exp_004
